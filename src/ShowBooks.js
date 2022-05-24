76764534436fe5655694d89250ea11960aa9abc8
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-24 11:03:15 -0500

    added more to file header

--- src/ShowBooks.js
+++ src/ShowBooks.js
@@ -1,34 +1,31 @@
 /**
  * @file ShowBooks.js
- *
+ * @authors @catdude2000 @Jacks-01
+ * @description 
  */
 import React from 'react';
 import Books from './Books';
 
+class ShowBooks extends React.Component {
+	handleSubmission = (evt) => {
+		evt.preventDefault();
+		const location = evt.target.location.value;
+		console.log(location, 'location');
+		this.props.fetchBooks(location);
+	};
 
-class ShowBooks extends React.Component{
-
-handleSubmission =(evt)=>{
-  evt.preventDefault();
-  const location= evt.target.location.value;
-  console.log(location, 'location');
-  this.props.fetchBooks(location);
-}
-
-  render(){
-
-    return(
-      <div>
-        <Books book={this.props.book}/>
-        <h1>filter by location</h1>
-        <form onSubmit={this.handleSubmission}>
-          <input name='location'/>
-          <button>enter</button>
-        </form>
-      </div>
-    )
-
-  }
+	render() {
+		return (
+			<div>
+				<Books book={this.props.book} />
+				<h1>filter by location</h1>
+				<form onSubmit={this.handleSubmission}>
+					<input name="location" />
+					<button>enter</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default ShowBooks;
